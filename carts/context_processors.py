1107ee5d61552194828e901ac50fb40e3443e751
marianaarevh <marianaarevalo933@gmail.com>
2022-11-28 01:38:57 -0700

    segundo commit

---
+++ carts/context_processors.py
@@ -0,0 +1,18 @@
+# para calcular la cantidad de productos en el carrito
+from .models import Cart, CartItem
+from .views import _cart_id
+
+def counter(request):
+    cart_count = 0
+    if 'admin' in request.path:
+        return {}
+    else:
+        try:
+            #_cart_id es el que tiene el numero de sesion
+            cart = Cart.objects.filter(cart_id=_cart_id(request))
+            cart_items = CartItem. objects.all().filter(cart=cart[:1])
+            for cart_item in cart_items:
+                cart_count += cart_item.quantity
+        except Cart.DoesNotExist:
+            cart_count = 0
+    return dict(cart_count = cart_count)
--- store/views.py
+++ store/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
-
-
+from carts.views import _cart_id
+from carts.models import CartItem
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
+from django.db.models import Q
 # Create your views here.
 
 def store(request, category_slug=None):
@@ -10,27 +13,51 @@ def store(request, category_slug=None):
     products = None
 
     if category_slug != None:
-        categories= get_object_or_404(Category, slug=category_slug)
+        categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('product_name')
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-    'products': products,
-    'product_count': product_count,
+        'products': paged_products,
+        'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
 
+
+#pasar los productos y el numero de prod que queremos mostrar
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        # si existe en el carrito devuelve True
+        in_cart= CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET: #keyword presente en la url??
+        keyword = request.GET['keyword'] #guardar ese valor
+        if keyword: #si no esta vacio buscamos en la bd
+            products=Product.objects.order_by('modified_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))#busca la palabra en el nombre o en la descripcion
+            product_count = products.count()
+    context = {
+        'products':products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
