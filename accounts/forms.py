87b081abea8bff8c0a6cf2074a23a179162eef77
marianaarevh <marianaarevalo933@gmail.com>
2022-11-29 03:28:02 -0700

    login, registro y logout

---
+++ accounts/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Account
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+    'placeholder': 'Ingrese contraseña',
+    'class': 'form-control'
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+    'placeholder': 'Confirmar contraseña'
+    }))
+    class Meta:
+        model=Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder']='Ingrese nombre'
+        self.fields['last_name'].widget.attrs['placeholder']='Ingrese apellido'
+        self.fields['phone_number'].widget.attrs['placeholder']='Ingrese numero de celular'
+        self.fields['email'].widget.attrs['placeholder']='Ingrese email'
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean() #super para modificar
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Las contraseñas no coinciden"
+            )
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from .models import Account
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid(): #       q tiene todos los campos llenos
+            first_name = form.cleaned_data['first_name'] ## fetch values del request
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0] #tomar la primera parte del email para el username
+
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'Registro exitoso, cuenta creada :)')
+            return redirect('register')
+
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/register.html', context)
 # Create your views here.
+def login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            #messages.success(request, 'Listo! ya ingresaste')
+            return redirect('home')
+        else:
+            messages.error(request, 'Email o Contraseña incorrectos')
+            return redirect('login')
+
+    return render(request, 'accounts/login.html')
+
+@login_required(login_url='login') #se tiene que haber iniciado sesion para poder cerrarla
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'Tu sesión ha sido cerrada')
+    return redirect('login')
